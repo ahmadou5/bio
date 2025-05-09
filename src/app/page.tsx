59375fe376c3114c89ffbc32f544efb5dc221c9b
ahmadou5 <ahmadlasauwal@gmail.com>
2025-05-09 03:00:59 +0100

    refactor: 39hrs 33min to submision

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import React from "react";
-import WalletApp from "../components/wallet";
-
-import Authentication from "../components/core/Auth/Authentication";
 import { useAuthStore } from "../store/authStore";
+import dynamic from "next/dynamic";
 
 export default function Home() {
   const { isAuthenticate } = useAuthStore();
+  const WalletNoSSR = dynamic(() => import("../components/wallet"), {
+    ssr: false,
+  });
+
+  const AuthNoSSR = dynamic(
+    () => import("../components/core/Auth/Authentication"),
+    {
+      ssr: false,
+    }
+  );
 
   return (
     <div className="h-full lg:w-full w-full">
-      {isAuthenticate ? <WalletApp /> : <Authentication />}
+      {isAuthenticate ? <WalletNoSSR /> : <AuthNoSSR />}
     </div>
   );
 }
